9a45fc7e45f20038b8e3a38cc430256f9c377dc9
Akhil Metukuru <145030279+akhilmet@users.noreply.github.com>
2024-07-23 06:57:38 +0000

    email commit

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:example@gmail.com">
-          example@gmail.com
+        <a className="underline" href="akhil.metukuru2016@gmail.com">
+        akhil.metukuru2016@gmail.com
         </a>{" "}
         or through this form.
       </p>
