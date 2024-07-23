9082e2e12b99df36bb44cc2be2625f57dfb973c6
Akhil Metukuru <145030279+akhilmet@users.noreply.github.com>
2024-07-23 05:24:12 +0000

    intro revert commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -104,7 +104,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com/akhmet"
+          href="https://github.com/akhilmet"
           target="_blank"
         >
           <FaGithubSquare />
