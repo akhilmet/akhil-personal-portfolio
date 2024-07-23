f90552369abe0f74048f18a9074155292eef38a3
Akhil Metukuru <145030279+akhilmet@users.noreply.github.com>
2024-07-23 04:51:32 +0000

    about edit commit

--- components/about.tsx
+++ components/about.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
+import Typing from 'react-typing-animation';
 
 export default function About() {
   const { ref } = useSectionInView("About");
@@ -18,32 +19,23 @@ export default function About() {
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
-      <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
-        <span className="font-medium">full-time position</span> as a software
-        developer.
-      </p>
+      <Typing>
+        <p className="mb-3">
+          I'm a rising junior at the University of Maryland, passionate about leveraging{" "}
+          <span className="font-medium">AI and software engineering</span> to create innovative solutions.
+          My journey in tech is driven by a curiosity to explore the boundless possibilities of{" "}
+          <span className="font-medium">machine learning</span> and entrepreneurship.
+          Currently, I'm honing my skills as a{" "}
+          <span className="font-medium">Software Engineering Intern at Halvik</span>, focusing on{" "}
+          <span className="font-medium">Generative AI applications</span>.
+        </p>
 
-      <p>
-        <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
-      </p>
+        <p>
+          <span className="italic">When I'm not coding</span>, you can find me weightlifting, running outdoors,
+          experimenting with new recipes in the kitchen, or spending time with foster cats.
+          I'm always looking to learn new technologies and improve my skills.
+        </p>
+      </Typing>
     </motion.section>
   );
 }
