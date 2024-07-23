2abf9a173c02d9f48fac5fca2bb450c1097cdf11
Akhil Metukuru <145030279+akhilmet@users.noreply.github.com>
2024-07-23 19:40:22 +0000

    intro edit commit

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,12 +62,14 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Akhil Metukuru.</span> I'm a{" "}
+        <span className="font-bold">Hello, I'm Akhil Metukuru.</span>
+        <br />
+        I'm a{" "}
         <TypeAnimation
           sequence={[
             'Software Engineer',
             2000,
-            'Full Stack Developer',
+            'Full-Stack Developer',
             2000,
             'Problem-Solver',
             2000,
